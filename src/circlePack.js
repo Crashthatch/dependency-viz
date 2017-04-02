95e042db2ccde5464938aadd3448cd6c03acd0b9
Tom Fletcher <fletcher.tom@gmail.com>
2017-04-02 20:10:42 +0100

    Add circlePack.

---
+++ src/circlePack.js
@@ -0,0 +1,85 @@
+import * as d3 from "d3";
+import { transitionTime } from "./config";
+
+var svg, data, g, diameter;
+var margin = 20;
+
+var color = d3.scaleLinear()
+  .domain([-1, 5])
+  .range(["hsl(25,70%,40%)", "hsl(25,50%,10%)"])
+  .interpolate(d3.interpolateHcl);
+
+export function initialize(svgg, hierarchy){
+  svg = svgg;
+  data = hierarchy;
+
+  diameter = +Math.min(svg.attr("width"), svg.attr("height"));
+
+  g = svg.append("g").attr('id', 'pack').attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+  updatePack();
+}
+
+export function updatePack(){
+
+  //Create tree layout
+  var pack = d3.pack()
+    .size([diameter - margin, diameter - margin])
+    .padding(2);
+
+  //Put data into layout.
+  var root = pack(data);
+
+  var focus = root,
+    nodes = pack(root).descendants(),
+    view;
+
+  var circle = g.selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+    .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+    .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+    .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
+
+  var text = g.selectAll("text")
+    .data(nodes)
+    .enter().append("text")
+    .attr("class", "label")
+    .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
+    .style("display", function(d) { return d.parent === root ? "inline" : "none"; })
+    .text(function(d) {
+      if(d.data.dependency){
+        return d.data.dependency.project_name;
+      }
+    });
+
+  var node = g.selectAll("circle,text");
+
+  svg
+    .on("click", function() { zoom(root); });
+
+  zoomTo([root.x, root.y, root.r * 2 + margin]);
+
+  function zoom(d) {
+    var focus0 = focus; focus = d;
+
+    var transition = d3.transition()
+      .duration(d3.event.altKey ? 7500 : 750)
+      .tween("zoom", function(d) {
+        var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+        return function(t) { zoomTo(i(t)); };
+      });
+
+    transition.selectAll("text")
+      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+      .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+      .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+      .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+  }
+
+  function zoomTo(v) {
+    var k = diameter / v[2]; view = v;
+    node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+    circle.attr("r", function(d) { return d.r * k; });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as d3 from "d3";
 import qwest from "qwest";
 import * as dependencyTree from "./dependencyTree";
+import * as circlePack from "./circlePack";
 
 // Import styles
 import './style.scss';
@@ -19,9 +20,13 @@ function ready(){
       if(project.dependencies.length > 0){
         return project.dependencies.filter( x => x); //Filter out nulls (Not sure why they are in there in the first place).
       }
-    });
+    })
+    .sum(function(d) { return 1; })
+    .sort(function(a, b) { return b.value - a.value; });
 
-    dependencyTree.initialize(svg, data);
+    //dependencyTree.initialize(svg, data);
+    circlePack.initialize(svg, data);
+
 
   })
   .catch(function(err){
