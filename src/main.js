2d785de8079b6282e9932f7c92ca8fc91f056be1
Tom Fletcher <fletcher.tom@gmail.com>
2017-04-14 17:33:03 +0100

    Enable switching between the 3 visualizations

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as d3 from "d3";
 import qwest from "qwest";
 import _ from 'lodash';
+import $ from 'jquery';
 import * as dependencyTree from "./dependencyTree";
 import * as circlePack from "./circlePack";
 import * as sunburst from "./sunburst";
@@ -28,14 +29,27 @@ function ready(){
     .sum(function(d) { return 1; });
 
     //dependencyTree.initialize(svg, data);
-    //circlePack.initialize(svg, data);
-    sunburst.initialize(svg, _.cloneDeep(data));
+    circlePack.initialize(svg, data);
+    //sunburst.initialize(svg, _.cloneDeep(data));
 
 
     //Create thumbnail selectors:
     /*dependencyTree.initialize(d3.select('.tree-selector svg'), _.cloneDeep(data));
     circlePack.initialize(d3.select('.pack-selector svg'), _.cloneDeep(data));
     sunburst.initialize(d3.select('.sun-selector svg'), _.cloneDeep(data));*/
+
+    $('.tree-selector').on('click', function(){
+      svg.selectAll("*").remove();
+      dependencyTree.initialize(svg, _.cloneDeep(data));
+    });
+    $('.pack-selector').on('click', function(){
+      svg.selectAll("*").remove();
+      circlePack.initialize(svg, _.cloneDeep(data));
+    });
+    $('.sun-selector').on('click', function(){
+      svg.selectAll("*").remove();
+      sunburst.initialize(svg, _.cloneDeep(data));
+    });
 
 
   })
