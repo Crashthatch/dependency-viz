b69f9f436a7bd0e922d4921a564737d433795b1e
Tom Fletcher <fletcher.tom@gmail.com>
2017-04-18 22:36:00 +0100

    Add build-and-deploy script for faster deployments.

---
+++ build-and-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Shortcut for easy deployment by Tom. If you're not Tom, you probably shouldn't run this script.
+
+aws ecr get-login --region us-east-1 | bash
+aws ecr get-login --region us-east-1 | sed s/docker/hyper/ | bash
+
+docker build -t depviz .
+docker tag depviz:latest 016279857314.dkr.ecr.us-east-1.amazonaws.com/depviz:latest
+docker push 016279857314.dkr.ecr.us-east-1.amazonaws.com/depviz:latest
+
+hyper pull 016279857314.dkr.ecr.us-east-1.amazonaws.com/depviz:latest
+hyper stop depviz
+hyper rm depviz
+hyper run -d --name depviz --env-file .env --env PORT=80 --env VIRTUAL_HOST=dependencyviz.crashthatch.com -p 80:80 016279857314.dkr.ecr.us-east-1.amazonaws.com/depviz:latest
+
+# Restarts the load balancer in front of all my websites:
+../hyper-load-balancer-lb.sh
